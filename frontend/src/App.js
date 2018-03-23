c415c373419f83c07bcbf76f47ea4106b797f9d0
ninanung <ninanung@naver.com>
2018-03-23 12:31:40 +0900

    frontend react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './icon.png';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +7,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="buttons">
+            <button className="btn btn-default button">로그인</button>
+            <button className="btn btn-default button">계정생성</button>
+          </div>
+          <div className="empty-div"></div>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"><a href="/">RememberMe</a></h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="text-bundle">
+          <p className="App-intro">
+            우리는 당신의 계정을 기억합니다.
+          </p>
+          <p className="App-intro">
+            우리는 당신의 계정을 알려드립니다.
+          </p>
+          <p className="App-intro">
+            원한다면 언제든지 기억합니다.
+          </p>
+          <p className="App-intro">
+            하지만 오직 당신에게만 알려드립니다.
+          </p>
+          <p className="App-intro">
+            <a href="/readme">사용법을 알고싶으신가요?</a>
+          </p>
+        </div>
+        <div className="App-footer">
+          <p className="App-footer-text">
+            Contact Us:
+            <br/>
+            ninanung@naver.com
+          </p>
+          <p className="App-footer-text">
+            Source Code:
+            <br/>
+            https://github.com/2018capstone/RememberMe
+          </p>
+        </div>
       </div>
-    );
+    )
   }
 }
 
