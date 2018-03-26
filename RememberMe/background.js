bd6157956949113957382e504ac848e701e4c957
ninanung <ninanung@naver.com>
2018-03-27 00:33:19 +0900

    finally calling once

--- RememberMe/background.js
+++ RememberMe/background.js
@@ -7,14 +7,38 @@ chrome.webRequest.onBeforeRequest.addListener(
     }, {urls: ["<all_urls>"]}, ["requestBody"]
 );
 
-chrome.tabs.onUpdated.addListener(
-    function() {
-        const urltext = document.getElementById("urltext");
-        chrome.tabs.getSelected(null,function(tab) {
-            urltext.innerText = tab.url;
-            chrome.storage.sync.set({ "rememberurl": tab.url }, function() {
-                console.log("url is " + tab.url);
-            });
+//chrome.tabs.onUpdated.addListener(
+//    function() {
+//        console.log("1")
+//        chrome.tabs.getSelected(null, function(tab) {
+//            console.log("2")
+//            chrome.storage.sync.set({ "rememberurl": tab.url }, function() {
+//                console.log("url is " + tab.url);
+//            });
+//            console.log("3")
+//            chrome.storage.sync.get(["rememberurl"], function(result) {
+//                console.log("get url is" + result.rememberurl);
+//            });
+//            console.log("4")
+//        });
+//        console.log("5")
+//    }
+//);
+
+let url = "";
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    areyou(tab.url);
+}); 
+
+const areyou = function(innerurl) {
+    if(innerurl !== url) {
+        url = innerurl;
+        chrome.storage.sync.set({ "rememberurl": innerurl }, function() {
+            console.log("url is " + innerurl);
+        });
+        chrome.storage.sync.get(["rememberurl"], function(result) {
+            console.log("get url is" + result.rememberurl);
         });
     }
-);
+}
