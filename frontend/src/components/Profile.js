859906493cd5e52b52978aa8b071a880bd4bbb41
Seungje Jun <ninanung@naver.com>
2018-05-15 19:05:27 +0900

    daily update

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Profile extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <h1>프로필 변경</h1>
+            </div>
         )
     }
 }
